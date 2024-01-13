ee2b392cca61051cd26ff49490b71c6bb6760c5b
ErmaIOS <tanikulovermekios@gmail.com>
2024-01-13 19:16:00 +0600

    Initial Commit

---
+++ hw_1_4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw_1_4
+//
+//  Created by Erma on 13/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
